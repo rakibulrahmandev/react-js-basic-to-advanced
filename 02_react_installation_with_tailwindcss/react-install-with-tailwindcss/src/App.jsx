3dbdf1112d694de38ed1490ea0fbe34a9ffca33f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-09 14:30:47 +0600

    react installation with tailwindcss

---
+++ 02_react_installation_with_tailwindcss/react-install-with-tailwindcss/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-gray-950 flex items-center justify-center">
+        <h1 className="text-6xl text-gray-700 font-bold">Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
