bd0a9825e56531baa3bcffe6989f2fb074637c1d
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-20 14:29:24 +0600

    react router. react dynamic routing

---
+++ 09_react_router/react-dynamic-routing/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router'
+import Header from "./components/Header/Header";
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Product from './components/Product/Product';
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import Contact from './components/Contact/Contact';
+import Error from './components/Error/Error';
+
+export default function App() {
+    return (
+        <>
+            <div className="w-full min-h-screen bg-gray-950 relative">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/product/:id" element={<ProductDetails />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/*" element={<Error />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
---
+++ 09_react_router/react-dynamic-routing/src/components/Error/Error.jsx
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+        <>
+            <div className="w-full text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <h1 className="text-6xl text-white font-bold">Oops 404</h1>
+            </div>
+        </>
+    )
+}
